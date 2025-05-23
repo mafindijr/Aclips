7be2e6f88fa958ec08ff38c108a5f845912dd784
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-23 14:44:39 -0700

    style: add background image to LandingPage component

--- src/components/landing-page.jsx
+++ src/components/landing-page.jsx
@@ -5,10 +5,11 @@ export default function LandingPage () {
 
 
     return (
-        <div className="landing-page">
+        <div className="landing-page" style={{backgroundImage: url('https://pikbest.com//backgrounds/secure-multi-currency-digital-wallets-for-cryptocurrency-and-online-payments_11711910.html')}}>
+
             <div>
                 
             </div>
         </div>
     );
-}
+} 
