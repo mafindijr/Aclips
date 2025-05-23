f6d3199795622337b943879d1fcb12a8b9735983
mafindijr <abdulrazaqiliyasumafindi@gmail.com>
2025-05-23 14:05:23 -0700

    style: add initial structure for LandingPage component in landing-page.jsx

---
+++ src/components/landing-page.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react';
+
+
+export default function LandingPage () {
+
+
+    return (
+        <div className="landing-page">
+            <div>
+                
+            </div>
+        </div>
+    );
+}
